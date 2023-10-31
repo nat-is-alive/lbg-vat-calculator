43ed195ecaa93fed9976792fe3ad743342bed525
Nathaniel Usherwood <nathanielusherwood@Nathaniels-MBP.lan>
2023-10-31 11:51:09 +0000

    third commit

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -5,7 +5,7 @@ pipeline {
     stage('Checkout') {
         steps {
           // Get some code from a GitHub repository
-          git branch: 'main', url: 'https://github.com/nat-is-alive/lbg-vat-calculator.git'
+          git branch: 'main', url: 'https://github.com/nat-is-alive/lbg-vat-calculator'
         }
     }
     stage('SonarQube Analysis') {
